a8288c3442a2bac0f7300072435d49ffeeaf32e8
Zoher Rangwala <153892122+zoherr@users.noreply.github.com>
2024-05-21 12:46:39 +0530

    Update googleanalytics.tsx

--- components/googleanalytics.tsx
+++ components/googleanalytics.tsx
@@ -6,6 +6,17 @@ import Script from 'next/script';
 const GoogleAnalytics = () => {
   return (
     <>
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-K8STFQWD"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
+      {/* End Google Tag Manager (noscript) */}
+
       <Script
         strategy='lazyOnload'
         src={`https://www.googletagmanager.com/gtag/js?id=G-TTSXXT2G23}`}
@@ -21,6 +32,13 @@ const GoogleAnalytics = () => {
               });
           `}
       </Script>
+
+      <Script id=''>{`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K8STFQWD');`}</Script>
+
     </>
   );
 };
